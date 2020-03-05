9a10086a891073c2e3910f1b9e8168a86e112c98
akash_10 <42974065+akashmishra96@users.noreply.github.com>
2020-03-05 12:46:10 +0530

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+//load our controllers 
 const user_controller = require('../controllers/user.controller');
 
 
-router.post('/create', user_controller.user_create);
+router.post('/create', user_controller.user_create);// create a user
 
-router.get('/:id', user_controller.user_details);
+router.get('/:id', user_controller.user_details);//get a user
 
-router.put('/:id/update', user_controller.user_update);
+router.put('/:id/update', user_controller.user_update);//update a user
 
-router.delete('/:id/delete', user_controller.user_delete);
+router.delete('/:id/delete', user_controller.user_delete);//delete a user
 
-
-
-
-
-// router.get('/test', product_controller.test);
  module.exports = router;
+
